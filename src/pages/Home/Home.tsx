f985f2db32be1be03682a20aaf204e7a34e4cf91
Timothée VANNIER <tva@subteno.com>
2023-03-31 17:26:52 +0200

    [IMP] reworked hero banner

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import Hero from "../../components/Hero";
 import Locations from "../../components/Locations";
+import image from '/hero.png'
 
 export default function Home() {
 	return (
 		<div>
-			<Hero/>
+			<Hero text={"Chez vous, partout et ailleurs"} image={image}/>
 			<Locations/>
 		</div>
 	)
