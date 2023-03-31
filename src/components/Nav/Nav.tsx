5726b7ba7653261833686e1000fd7abc81d4975f
Timothée VANNIER <tva@subteno.com>
2023-03-31 14:24:02 +0200

    [IMP] clickable logo

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -11,7 +11,7 @@ export default function Nav({children = undefined}: Props) {
 	return (
 		<>
 			<nav className={"flex justify-between py-40"}>
-				<img src={logo} alt="logo" className={"w-210"}/>
+				<NavLink to={LINKS.home.to}><img src={logo} alt="logo" className={"w-210"}/></NavLink>
 				<ul className={"text-primary flex justify-between items-center w-250"}>
 					{Object.values(LINKS).map(({to, text, noNavBar}) => (
 						!noNavBar && (<li key={to} className={"list-none"}>
