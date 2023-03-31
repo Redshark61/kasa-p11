40a8449991ef3081d61e22fcf1cb0f9f50da0290
Timothée VANNIER <tva@subteno.com>
2023-03-31 17:25:23 +0200

    [IMP] routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Nav from "./components/Nav";
 import {LINKS} from "./constants";
 import Location from "./pages/Location";
 import Footer from "./components/Footer";
+import Error404 from "./pages/404";
+import About from "./pages/About";
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
 					<Routes>
 						<Route path={LINKS.home.to} element={<Home/>}/>
 						<Route path={LINKS.location.to} element={<Location/>}/>
+						<Route path={LINKS.about.to} element={<About/>}/>
+						<Route path={"*"} element={<Error404/>}/>
 					</Routes>
 				</Nav>
 			</Base>
