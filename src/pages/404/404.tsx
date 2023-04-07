3aa5877dfd908e8a0ac33f245f2e95eaf858b33c
Timothée VANNIER <tva@subteno.com>
2023-04-07 13:24:10 +0200

    [IMP] page 404 responsive

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,12 +1,13 @@
 import {Link} from "react-router-dom";
 import {LINKS} from "../../constants";
+import className from './index.module.css'
 
 export default function Error404() {
 	return (
-		<div className={"text-primary flex justify-center items-center flex-col"}>
-			<h1 style={{fontSize: 288}}>404</h1>
-			<h2 className={"text-36 font-500"}>Oups ! La page que vous demander n'existe pas.</h2>
-			<Link to={LINKS.home.to} className={"text-18 font-500"} style={{marginTop: 180}}>Retourner sur la page
+		<div className={`${className.error404} text-primary flex justify-center items-center flex-col h-full`}>
+			<h1 style={{fontSize: "20vw"}}>404</h1>
+			<h2 className={"text-36 font-500 text-center"}>Oups ! La page que vous demander n'existe pas.</h2>
+			<Link to={LINKS.home.to} className={"text-18 font-500"}>Retourner sur la page
 				d'accueil</Link>
 		</div>
 	);
