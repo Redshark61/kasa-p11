8dec2a55844b9763e80091638a8b2e5ac10e2286
Timothée VANNIER <tva@subteno.com>
2023-04-07 11:47:12 +0200

    [FIX] double quotes + semicolon

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,12 +1,11 @@
 import {useEffect, useState} from "react";
 import {CarouselArrows} from "./CarouselArrows";
-import className from './index.module.css'
+import className from "./index.module.css";
 import Spinner from "../Spinner";
 
 interface CarouselProps {
 	pictures: string[];
 }
-
 
 export default function Carousel({pictures}: CarouselProps) {
 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -16,60 +15,75 @@ export default function Carousel({pictures}: CarouselProps) {
 	const cacheImages = async (images: string[]) => {
 		const promises = images.map((image) => {
 			return new Promise((resolve, reject) => {
-				const img = new Image()
-				img.src = image
-				img.onload = resolve
-				img.onerror = reject
-			})
-		})
+				const img = new Image();
+				img.src = image;
+				img.onload = resolve;
+				img.onerror = reject;
+			});
+		});
 		promises.forEach((promise) => {
 			promise.catch((error) => {
-				setErrorURLs((prevState) => [...prevState, error.target.src])
-			})
-		})
-		setIsLoading(false)
-	}
+				setErrorURLs((prevState) => [...prevState, error.target.src]);
+			});
+		});
+		setIsLoading(false);
+	};
 
 	useEffect(() => {
-		cacheImages(pictures)
-	}, [pictures])
+		cacheImages(pictures);
+	}, [pictures]);
 
 	const handleClick = ({delta}: { delta: 1 | -1 }) => {
 		if (currentImageIndex === pictures.length - 1 && delta === 1) {
-			setCurrentImageIndex(0)
+			setCurrentImageIndex(0);
 		} else if (currentImageIndex === 0 && delta === -1) {
-			setCurrentImageIndex(pictures.length - 1)
+			setCurrentImageIndex(pictures.length - 1);
 		} else {
-			setCurrentImageIndex((prevState) => prevState + delta)
+			setCurrentImageIndex((prevState) => prevState + delta);
 		}
-	}
+	};
 
 	const hasError = errorURLs.includes(pictures[currentImageIndex]);
 
 	if (isLoading) {
-		return <Spinner/>
+		return <Spinner/>;
 	}
 
 	return (
-		<div className={`w-full bg-pos-center relative bg-size-cover radius-25 ${className.carrousel}`}>
-			{hasError ?
-				<div className={"w-full h-full radius-25 flex justify-center items-center"}><h1>Error</h1></div> :
-				<img src={pictures[currentImageIndex]} alt={"location"} className={"w-full h-full radius-25"}
-					 style={{objectFit: "cover"}}/>}
+		<div
+			className={`w-full bg-pos-center relative bg-size-cover radius-25 ${className.carrousel}`}
+		>
+			{hasError ? (
+				<div className={"w-full h-full radius-25 flex justify-center items-center"}>
+					<h1>Error</h1>
+				</div>
+			) : (
+				<img
+					src={pictures[currentImageIndex]}
+					alt={"location"}
+					className={"w-full h-full radius-25"}
+					style={{objectFit: "cover"}}
+				/>
+			)}
 			<div
 				className={"w-full h-full absolute radius-25"}
 				style={{
 					top: 0,
-					backgroundImage: "linear-gradient(90deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0) 15%, rgba(0,0,0,0) 85%, rgba(0,0,0,0.5) 100%)"
+					backgroundImage:
+						"linear-gradient(90deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0) 15%, rgba(0,0,0,0) 85%, rgba(0,0,0,0.5) 100%)",
 				}}
 			/>
-			{pictures.length > 1 &&
+			{pictures.length > 1 && (
 				<>
 					<CarouselArrows handleClick={handleClick}/>
-					<span className={"absolute text-18 font-500 text-white"}
-						  style={{bottom: 25, right: 25}}>{currentImageIndex + 1}/{pictures.length}</span>
+					<span
+						className={"absolute text-18 font-500 text-white"}
+						style={{bottom: 25, right: 25}}
+					>
+						{currentImageIndex + 1}/{pictures.length}
+					</span>
 				</>
-			}
+			)}
 		</div>
-	)
+	);
 }
--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -1,52 +1,59 @@
-import {useLoaderData, useParams} from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import Carousel from "../../components/Carousel";
 import Tags from "../../components/Tags";
 import Dropdown from "../../components/Dropdpown";
 import Error404 from "../404";
 import Rating from "../../components/Rating";
 import ProfileThumbnail from "../../components/ProfileThumbnail";
-import className from './index.module.css'
-import {LocationType} from "../../types";
+import className from "./index.module.css";
+import { LocationType } from "../../types";
 
 export default function Location() {
-	const {id} = useParams<{ id: string }>()
-	const locationData = useLoaderData() as LocationType[]
-	console.log(locationData)
-	const location = locationData.find(location => location.id === id)
+	const { id } = useParams<{ id: string }>();
+	const locationData = useLoaderData() as LocationType[];
+	const location = locationData.find((location) => location.id === id);
 	if (!location) {
-		return <Error404/>
+		return <Error404 />;
 	}
 
 	return (
 		<>
-			<Carousel pictures={location.pictures}/>
+			<Carousel pictures={location.pictures} />
 			<div className={`flex justify-between mt-30 ${className.description}`}>
 				<div>
 					<h1 className={"font-300 text-36 text-primary"}>{location.title}</h1>
 					<h2 className={"font-300 text-18 text-primary"}>{location.location}</h2>
-					<Tags className={className.tags} tags={location.tags}/>
+					<Tags className={className.tags} tags={location.tags} />
 				</div>
 				<div className={className.host_information}>
-					<div className={'flex items-center text-primary mb-30'}>
+					<div className={"flex items-center text-primary mb-30"}>
 						<h3 className={"text-18 font-500 mr-10"}>{location.host.name}</h3>
-						<ProfileThumbnail minSize={"32px"} size={"4.4vw"} url={location.host.picture}
-										  alt={location.host.name}/>
+						<ProfileThumbnail
+							minSize={"32px"}
+							size={"4.4vw"}
+							url={location.host.picture}
+							alt={location.host.name}
+						/>
 					</div>
-					<Rating value={location.rating}/>
+					<Rating value={location.rating} />
 				</div>
 			</div>
 			<div className={`${className.dropdown_container} flex gap-80 mt-25`}>
 				<Dropdown title={"Description"} className={`w-half`}>
-					<p className={"text-18"} style={{lineHeight: "max(17px, 1.7vw)"}}>{location.description}</p>
+					<p className={"text-18"} style={{ lineHeight: "max(17px, 1.7vw)" }}>
+						{location.description}
+					</p>
 				</Dropdown>
 				<Dropdown title={"Équipements"} className={`w-half`}>
-					<ul style={{lineHeight: "max(17px, 1.7vw)"}}>
-						{location.equipments.map(equipment => (
-							<li className={"list-none text-18"} key={equipment}>{equipment}</li>
+					<ul style={{ lineHeight: "max(17px, 1.7vw)" }}>
+						{location.equipments.map((equipment) => (
+							<li className={"list-none text-18"} key={equipment}>
+								{equipment}
+							</li>
 						))}
 					</ul>
 				</Dropdown>
 			</div>
 		</>
-	)
+	);
 }
